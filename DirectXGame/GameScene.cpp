db110024381c66aea4cdf76e7be168638dafda04
yaritakoya <k024g0053@g.neec.ac.jp>
2025-04-14 15:00:18 +0900

    KamataEngineを入れた

---
+++ DirectXGame/GameScene.cpp
@@ -0,0 +1,13 @@
+#include "GameScene.h"
+
+void GameScene::Initialize() {
+
+}
+
+void GameScene::Update() {
+
+}
+
+void GameScene::Draw() {
+
+}
---
+++ DirectXGame/main.cpp
@@ -0,0 +1,50 @@
+#include "GameScene.h"
+#include <KamataEngine.h>
+#include <Windows.h>
+
+using namespace KamataEngine;
+
+// Windowsアプリでのエントリーポイント(main関数)
+int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
+
+	// エンジンの初期化
+	KamataEngine::Initialize(L"LE2D_24_ヤリタ_コウヤ_AL3");
+
+	// DirectXCommonインスタンスの取得
+	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
+
+	//ゲームシーンのインスタンス生成
+	GameScene* gameScene = new GameScene();
+	//ゲームシーンの初期化
+	gameScene->Initialize();
+
+	// メインループ
+	while (true) {
+		// エンジンの更新
+		if (KamataEngine::Update()) {
+			break;
+		}
+
+		//ゲームシーンの更新
+		gameScene->Update();
+
+		// 描画開始
+		dxCommon->PreDraw();
+
+		//ゲームシーンの描画
+		gameScene->Draw();
+
+		// 描画終了
+		dxCommon->PostDraw();
+	}
+
+	//ゲームシーンの開放
+	delete gameScene;
+	//nullptrの代入
+	gameScene = nullptr;
+
+	// エンジンの終了処理
+	KamataEngine::Finalize();
+
+	return 0;
+}
