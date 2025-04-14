c369c6a815106d577e306a8304079f7870a68d0f
yaritakoya <k024g0053@g.neec.ac.jp>
2025-04-14 16:56:29 +0900

    2D,3Dモデルの実装

--- DirectXGame/GameScene.cpp
+++ DirectXGame/GameScene.cpp
@@ -1,13 +1,53 @@
 #include "GameScene.h"
 
-void GameScene::Initialize() {
+using namespace KamataEngine;
 
+GameScene::~GameScene() {
+	delete sprite_;
+	delete model_;
+}
+
+void GameScene::Initialize() {
+	// ファイル名を指定してテクスチャを読み込む
+	textureHandle_ = TextureManager::Load("rikusakana.png");
+	// スプライトインスタンスの生成
+	sprite_ = Sprite::Create(textureHandle_, {100, 50});
+	// 3Dモデルの生成
+	model_ = Model::Create();
+	// ワールドトランスフォームの初期化
+	worldTransform_.Initialize();
+	// カメラの初期化
+	camera_.Initialize();
 }
 
 void GameScene::Update() {
-
+	// スプライトも今の座標を取得
+	Vector2 position = sprite_->GetPosition();
+	// 座標を｛ 2，1 ｝移動
+	position.x += 2.0f;
+	position.y += 1.0f;
+	// 移動した座標をスプライトに反映
+	sprite_->SetPosition(position);
 }
 
 void GameScene::Draw() {
+	// DirectXCommonインスタンスの取得
+	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
 
+	// スプライト描画前処理
+	Sprite::PreDraw(dxCommon->GetCommandList());
+	
+	sprite_->Draw();
+
+	// 3Dモデル描画前処理
+	Model::PreDraw(dxCommon->GetCommandList());
+
+	// 3Dモデル描画
+	model_->Draw(worldTransform_, camera_, textureHandle_);
+
+	// 3Dモデル描画後処理
+	Model::PostDraw();
+
+	// スプライト描画後処理
+	Sprite::PostDraw();
 }
--- DirectXGame/GameScene.h
+++ DirectXGame/GameScene.h
@@ -1,9 +1,14 @@
 #pragma once
 #include"KamataEngine.h"
 
+using namespace KamataEngine;
+
 //ゲームシーン
 class GameScene{
 public:
+	//
+	~GameScene();
+
 	//初期化
 	void Initialize();
 
@@ -12,4 +17,17 @@ public:
 
 	//描画
 	void Draw();
+
+private:
+	//テクスチャハンドル
+	uint32_t textureHandle_ = 0;
+	//スプライト
+	Sprite* sprite_ = nullptr;
+	//3Dモデル
+	Model* model_ = nullptr;
+	//ワールドトランスフォーム
+	WorldTransform worldTransform_;
+	//カメラ
+	Camera camera_;
+	
 };
